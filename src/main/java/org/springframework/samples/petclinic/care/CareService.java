6753ce72a4d3690ffcf67b2a7fddd3e40c28d3df
Jaortiz1 <juanantonioortizguerra@gmail.com>
2022-01-21 11:43:54 +0100

    primeros 5

--- src/main/java/org/springframework/samples/petclinic/care/CareService.java
+++ src/main/java/org/springframework/samples/petclinic/care/CareService.java
@@ -5,36 +5,39 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 @Service
-public class CareService {    
-
+public class CareService {
+	/*
+	 * Modificar la clase “CareService”, para que sea un servicio Spring de lógica
+	 * de negocio, que permita obtener todos cuidados realizados (CareProvision). No
+	 * modifique por ahora la implementación de los demás métodos del servicio.
+	 */
 	@Autowired
 	private CareProvisionRepository careRepository;
-    public List<Care> getAllCares(){
-        return this.careRepository.findAllCares();
-    }
 
-    public List<Care> getAllCompatibleCares(String petTypeName){
-        return null;
-    }
+	public List<Care> getAllCares() {
+		return this.careRepository.findAllCares();
+	}
 
-    public Care getCare(String careName) {
-        return null;
-    }
+	public List<Care> getAllCompatibleCares(String petTypeName) {
+		return null;
+	}
 
-    
-    public CareProvision save(CareProvision p) throws NonCompatibleCaresException, UnfeasibleCareException {
-        return null;   
-    }
+	public Care getCare(String careName) {
+		return null;
+	}
 
-    public List<CareProvision> getAllCaresProvided(){
-        return null;
-    }
+	public CareProvision save(CareProvision p) throws NonCompatibleCaresException, UnfeasibleCareException {
+		return null;
+	}
 
-    public List<CareProvision> getCaresProvided(Integer visitId){
-        return null;
+	public List<CareProvision> getAllCaresProvided() {
+		return this.careRepository.findAll();
+	}
 
-    }
-    
+	public List<CareProvision> getCaresProvided(Integer visitId) {
+		return null;
+
+	}
+
 }
